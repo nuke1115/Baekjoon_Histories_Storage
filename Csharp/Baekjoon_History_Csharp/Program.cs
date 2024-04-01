7e0ce47c16007801022a016745ce049ef28340d2
nuke1115 <nuclearbomb280@gmail.com>
2024-04-01 23:08:17 +0900

    test : solved 2557

---
+++ Csharp/Baekjoon_History_Csharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Baekjoon_History_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
