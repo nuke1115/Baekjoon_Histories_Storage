4f4802dedcbb1765bdfff7be9c320c54777a6626
nuke1115 <nuclearbomb280@gmail.com>
2024-04-02 20:33:37 +0900

    solved : solved 2501

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/2501.cs
@@ -0,0 +1,37 @@
+﻿namespace Fuc2
+{
+    public class Program
+    {
+        public static void aMain()
+        {
+            int[] nums = Array.ConvertAll(Console.ReadLine().Split(" ") , int.Parse);
+            int count = 0;
+            int result = 0;
+            int smallestMeasure = 0;
+
+            for (int i = 1; i <= nums[0]; i++)
+            {
+                result = nums[0] % i;
+
+
+                if (result == 0)
+                {
+                    count++;
+                }
+
+                if (count == nums[1])
+                {
+                    smallestMeasure = i;
+                    break;
+                }
+
+
+            }
+
+
+            Console.WriteLine(smallestMeasure);
+
+
+        }
+    }
+}
