07356db3611996f8ca8b0e8fa760adaddc13fd2c
nuke1115 <nuclearbomb280@gmail.com>
2024-04-03 20:15:53 +0900

    solved : solved 2010

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/2010.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Fuc3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            int multiTapHoles = 0;
+            int sum = 0;
+
+            for (int i = 1; i <= testCases; i++)
+            {
+                multiTapHoles = int.Parse(Console.ReadLine());
+
+                if (i != testCases)
+                {
+                    multiTapHoles--;
+                }
+
+                sum += multiTapHoles;
+
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
