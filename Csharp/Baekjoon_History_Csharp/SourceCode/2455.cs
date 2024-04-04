5ca9f5cefb7adba708740173d8024733f94eab0d
nuke1115 <nuclearbomb280@gmail.com>
2024-04-04 20:55:05 +0900

    solved : 2455

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/2455.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Fuc4
+{
+	public class Program
+	{
+		public static void aMain()
+		{
+			int highestPeopleCount = 0;
+			int[] nums = new int[2];
+			int nowPeopleCount = 0;
+
+			for (int i = 0; i < 4; i++)
+			{
+				nums = Array.ConvertAll(Console.ReadLine().Split(" ") , int.Parse);
+
+				nowPeopleCount += nums[1] - nums[0];
+
+				if (nowPeopleCount > highestPeopleCount)
+				{
+					highestPeopleCount = nowPeopleCount;
+				}
+
+			}
+
+			Console.WriteLine(highestPeopleCount);
+
+
+		}
+	}
+}
