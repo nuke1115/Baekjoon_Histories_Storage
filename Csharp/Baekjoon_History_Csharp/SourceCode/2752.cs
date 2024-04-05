d98fcc2ccd49f6534b87cec7a21e1f0aa396d2c1
nuke1115 <nuclearbomb280@gmail.com>
2024-04-05 23:10:17 +0900

    solved : 2752

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/2752.cs
@@ -0,0 +1,37 @@
+﻿namespace Fuc5
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			int[] nums = Array.ConvertAll(Console.ReadLine().Split(" ") , int.Parse);
+			int tmp = 0;
+			int highestNumIndex = 0;
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				highestNumIndex = i;
+
+				for (int k = i; k < nums.Length; k++)
+				{
+					if (nums[k] > nums[highestNumIndex])
+					{
+						highestNumIndex = k;
+					}
+				}
+
+				tmp = nums[highestNumIndex];
+				nums[highestNumIndex] = nums[i];
+				nums[i] = tmp;
+
+
+			}
+
+			for (int i = 2; i >= 0; i--)
+			{
+				Console.Write($"{nums[i]} ");
+			}
+
+		}
+	}
+}
