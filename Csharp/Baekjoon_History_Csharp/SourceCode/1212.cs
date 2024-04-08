75c590f07af2b1d51eb02b33924731e81fc5060c
nuke1115 <nuclearbomb280@gmail.com>
2024-04-08 21:38:02 +0900

    solved : 1212

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/1212.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Text;
+
+namespace Fuc_8
+{
+	public class Program
+	{
+		
+		public static void Main()
+		{
+
+			string[] binaryWithZero = { "001" , "010" , "011" , "100" , "101" , "110" , "111" , "000"};
+			string[] binaryWithOutZero = { "1", "10", "11", "100", "101", "110", "111" , "0"};
+
+			char[] octalNums = Console.ReadLine().ToCharArray();
+
+			StringBuilder result = new StringBuilder();
+
+			int index = (int)Char.GetNumericValue(octalNums[0]) - 1;
+
+			if (index >= 0)
+			{
+				result.Append(binaryWithOutZero[index]);
+			}
+			else
+			{
+				result.Append(binaryWithOutZero[7]);
+			}
+
+			for (int i = 1; i < octalNums.Length; i++)
+			{
+				index = (int)Char.GetNumericValue(octalNums[i]) - 1;
+				if (index >= 0)
+				{
+					result.Append(binaryWithZero[index]);
+				}
+				else
+				{
+					result.Append(binaryWithZero[7]);
+				}
+			}
+
+			Console.WriteLine(result.ToString());
+
+
+		}
+	}
+}
+
+//fuckkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkit
