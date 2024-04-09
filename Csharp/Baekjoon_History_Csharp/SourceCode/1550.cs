5ac1a3817859c6e4c7622e55dc1b81bc569cd9d7
nuke1115 <nuclearbomb280@gmail.com>
2024-04-09 19:34:39 +0900

    solved : 1550

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/1550.cs
@@ -0,0 +1,38 @@
+﻿namespace Fuc9
+{
+	public class Program
+	{
+		const int calibrationNum = 55;
+		public static void aMain()
+		{
+			/*
+			char[] inputHex = Console.ReadLine().ToCharArray();
+			int sum = 0;
+			int tmpNum = 0;
+
+			int exponent = 0;
+			for (int i = inputHex.Length - 1; i >= 0; i--)
+			{
+				if (Char.IsNumber(inputHex[i]))
+				{
+					tmpNum = (int)Char.GetNumericValue(inputHex[i]);
+				}
+				else
+				{
+					tmpNum = (int)inputHex[i] - calibrationNum;
+				}
+
+
+				sum += tmpNum*(int)Math.Pow(16,exponent);
+				exponent++;
+			}
+
+			Console.WriteLine(sum);
+			*/
+
+
+			string hexInput = Console.ReadLine();
+			Console.WriteLine(Convert.ToInt32(hexInput,16)); //이런게 있는줄 처음알았네...
+		}
+	}
+}
