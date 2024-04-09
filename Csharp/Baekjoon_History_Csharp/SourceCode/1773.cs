9a6043f848e32d15a76a99e9a1bc878fc4dff1ae
nuke1115 <nuclearbomb280@gmail.com>
2024-04-09 21:53:08 +0900

    solved : 1773

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/1773.cs
@@ -0,0 +1,39 @@
+﻿namespace Fuc10
+{
+	public class Program
+	{
+		public static void aMain()
+		{
+			string[] input = Console.ReadLine().Split(" ");
+			int studentCount = int.Parse(input[0]);
+			int time = int.Parse(input[1]);
+			
+			int[] students = new int[studentCount];
+			int sum = 0;
+			
+			for (int i = 0; i < students.Length; i++)
+			{
+				students[i] = int.Parse(Console.ReadLine());
+			}
+
+
+			int repeatNum = 0;
+			for (int i = 1; i <= time;  i++)
+			{
+				repeatNum = 0;
+				for (; repeatNum < students.Length; repeatNum++)
+				{
+					if (i % students[repeatNum] == 0)//연산
+					{
+						sum++;
+						break;
+					}
+				}
+			}
+
+			Console.WriteLine(sum);
+
+		}
+	}
+}
+//브루트 포-스
