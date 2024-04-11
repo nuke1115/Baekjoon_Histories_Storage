167aabd8c6651e90b8f09cfff4153ba9a11a9a1d
nuke1115 <nuclearbomb280@gmail.com>
2024-04-11 22:32:53 +0900

    solved : 4470

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/4470.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace Fuc14
+{
+	public class Program
+	{
+		public static void aMain()
+		{
+			int testCase = int.Parse(Console.ReadLine());
+			StringBuilder result = new StringBuilder();
+
+			for (int i = 1; i <= testCase; i++)
+			{
+				result.AppendLine($"{i}. {Console.ReadLine()}");
+			}
+			Console.WriteLine( result.ToString() );
+
+		}
+	}
+}
