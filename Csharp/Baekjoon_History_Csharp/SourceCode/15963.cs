068ce52a4c053c54828e0f88a4accef005dfb51c
nuke1115 <nuclearbomb280@gmail.com>
2024-04-15 18:42:57 +0900

    solved : 15963

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/15963.cs
@@ -0,0 +1,11 @@
+﻿namespace Fuc16
+{
+	public class Program
+	{
+		public static void aMain()
+		{
+			string[] args = Console.ReadLine().Split(" ");
+			Console.WriteLine(args[0] == args[1] ? 1 : 0);
+		}
+	}
+}
