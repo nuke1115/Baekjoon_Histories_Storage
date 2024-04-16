397723b77510c8b7003cfeb1313a6295a9d2016b
nuke1115 <nuclearbomb280@gmail.com>
2024-04-16 19:24:56 +0900

    solved : 1292

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/1292.cs
@@ -0,0 +1,39 @@
+﻿namespace Fuc19
+{
+	public class Program
+	{
+		const int numCap = 46;
+		public static void Main()
+		{
+
+			
+
+			string[] args = Console.ReadLine().Split(" ");
+
+			int posA = int.Parse(args[0]);
+			int posB = int.Parse(args[1]);
+
+
+			int sum = 0;
+			
+
+			int index = 1;
+
+
+			for (int i = 1; i <= numCap ; i++)
+			{
+				for (int k = 0; k < i  ; k++ , index++)
+				{
+					if (index >= posA && index <= posB)
+					{
+						sum += i;
+
+					}
+
+				}
+			}
+
+			Console.WriteLine(sum);
+		}
+	}
+}
