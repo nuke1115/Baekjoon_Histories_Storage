1b997ac0b1cd0aef27dd72cc2fb81014f23cbf9e
nuke1115 <nuclearbomb280@gmail.com>
2024-04-19 19:37:46 +0900

    solved : 1357

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/1357.cs
@@ -0,0 +1,28 @@
+﻿namespace Fuc22
+{
+	public class Program
+	{
+		public static void aMain()
+		{
+			string[] input = Console.ReadLine().Split(" ");
+
+			int numA = Rev(input[0].ToCharArray());
+			int numB = Rev(input[1].ToCharArray());
+
+
+
+			Console.WriteLine(Rev((numA + numB).ToString().ToCharArray()));
+
+
+		}
+
+		public static int Rev(char[] charArr)
+		{
+			Array.Reverse(charArr);
+			string fuck = new string(charArr);
+			int result = int.Parse(fuck);
+
+			return result;
+		}
+	}
+}
