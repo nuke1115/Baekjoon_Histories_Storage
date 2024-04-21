1e32eef90a11958c3be08dbf2bd32ee47d6eb6e3
nuke1115 <nuclearbomb280@gmail.com>
2024-04-21 13:15:55 +0900

    solved : 2609

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/2609.cs
@@ -0,0 +1,58 @@
+﻿namespace Fuc24
+{
+	public class Program
+	{
+		public static void aMain()
+		{
+			string[] input = Console.ReadLine().Split(" ");
+			int numA = int.Parse(input[0]);
+			int numB = int.Parse(input[1]);
+
+			int gcd = GetGCD(numA , numB);
+
+			Console.WriteLine(gcd);
+			Console.WriteLine((numA*numB)/gcd);
+
+
+
+
+
+		}
+
+
+		static int GetGCD(int numA, int numB)
+		{
+
+			
+			while (numA % numB != 0)
+			{
+				if (numA < numB)
+				{
+					int tmp = numA;
+					numA = numB;
+					numB = tmp;
+				}
+
+				numA %= numB;
+
+			}
+
+			return numB;
+
+
+		}
+	}
+}
+
+//유
+//클
+//리
+//드
+//호
+//제
+//법
+//아
+//시
+//는
+//구
+//나
