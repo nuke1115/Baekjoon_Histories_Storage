17387bf0f56eb43c7bea499e5f27aca35e694ac3
nuke1115 <nuclearbomb280@gmail.com>
2024-04-22 19:00:55 +0900

    solved : 4153

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/4153.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Fuc25_1
+{
+	public class Program
+	{
+		const int EOF = 0;
+		public static void Main()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			int[] nums = new int[3];
+
+			while (true)
+			{
+				nums = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+				if (nums[0] == EOF)
+				{
+					break;
+				}
+				Array.Sort(nums);
+
+				sb.AppendLine(Math.Pow(nums[0], 2) + Math.Pow(nums[1], 2) == Math.Pow(nums[2], 2) ? "right" : "wrong");
+
+
+
+			}
+			Console.WriteLine(sb.ToString());
+
+		}
+	}
+}
