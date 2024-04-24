a4b3dd562400d62799aeba6fb184476823c561f0
nuke1115 <nuclearbomb280@gmail.com>
2024-04-24 18:53:30 +0900

    solved? : 2869

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/2869_FUCK.cs
@@ -0,0 +1,23 @@
+﻿namespace Fuc27
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			string[] args = Console.ReadLine().Split(" ");
+			int mA = int.Parse(args[0]);
+			int mB = int.Parse(args[1]);
+			int mV = int.Parse(args[2]);
+
+			int goal = mV - mA;
+			int canUp = mA - mB;
+
+			Console.WriteLine(Math.Ceiling((double)goal/canUp) + 1);
+
+
+		}
+	}
+}
+//fuck
+//math
+//fuck
