ac77b779a5dffdf2e9b864630d49b486194042a0
nuke1115 <nuclearbomb280@gmail.com>
2024-04-25 21:32:26 +0900

    solved : 1236 L

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/1236_v1.cs
@@ -0,0 +1,63 @@
+﻿namespace Fuc29_v1
+{
+	public class Program
+	{
+		const char compareKeyExist = 'X';
+		
+		public static void aMain()
+		{
+			int x = 0;
+			int y = 0;
+
+			string[] args = Console.ReadLine().Split(" ");
+
+			y = int.Parse(args[0]);
+			x = int.Parse(args[1]);
+
+			
+			char[,] castle = new char[y,x];
+			bool[] rowX = new bool[x];
+			bool[] rowY = new bool[y];
+
+			int sumX = x;
+			int sumY = y;
+
+			for (int depth = 0; depth < y; depth++)
+			{
+				char[] input = Console.ReadLine().ToCharArray();
+
+				for (int width = 0; width < x; width++)
+				{
+					castle[depth, width] = input[width];
+					if (input[width] == compareKeyExist)
+					{
+						if (!rowX[width])
+						{
+							rowX[width] = true;
+							sumX--;
+						}
+					}
+
+				}
+			}
+
+			for (int width = 0; width < x; width++)
+			{
+				for (int depth = 0; depth < y; depth++)
+				{
+					if (castle[depth,width] == compareKeyExist)
+					{
+						if (!rowY[depth])
+						{
+							rowY[depth] = true;
+							sumY--;
+						}
+					}
+				}
+			}
+
+			Console.WriteLine(sumX > sumY ? sumX : sumY);
+
+		}
+	}
+}
