5e3875e748a14d4f03debfda94a0dd6c0e562f57
nuke1115 <nuclearbomb280@gmail.com>
2024-04-26 20:59:58 +0900

    solved : 10798

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/10798_v1.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace Fuc30_v1
+{
+	public class Program
+	{
+		public static void aMain()
+		{
+			int[] charsCounts = new int[5];
+
+			List<char>[] lists = new List<char>[5];
+			
+
+
+			StringBuilder sb = new StringBuilder();
+
+
+			for (int i = 0; i < 5; i++)
+			{
+				char[] inputTmp = Console.ReadLine().ToCharArray();
+
+				lists[i] = new List<char>();
+				lists[i].AddRange(inputTmp);
+
+				charsCounts[i] = inputTmp.Length;
+			}
+
+
+			int repeatNum = charsCounts.Max();
+			for (int i = 0; i < repeatNum; i++)
+			{
+				for (int k = 0; k < lists.Length; k++)
+				{
+					if (lists[k].Count - 1 >= i)
+					{
+						sb.Append(lists[k][i]);
+					}
+				}
+			}
+			Console.WriteLine(sb.ToString());
+
+		}
+	}
+}
---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/10798_v2.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Fuc30_v2
+{
+	public class Program
+	{
+		public static void aMain()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			string[] strings = new string[5];
+			int repeatNums = 0;
+
+			for (int i = 0; i < 5; i++)
+			{
+				strings[i] = Console.ReadLine();
+				
+				if (strings[i].Length > repeatNums)
+				{
+					repeatNums = strings[i].Length;
+				}
+
+			}
+
+			
+			for (int i = 0; i < repeatNums; i++)
+			{
+				for (int k = 0; k < 5; k++)
+				{
+					if (strings[k].Length - 1>= i)
+					{
+						sb.Append(strings[k][i]);
+					}
+				}
+			}
+
+
+
+
+			Console.WriteLine(sb.ToString());
+		}
+	}
+}
