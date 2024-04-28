e4d8bf11abf0cfb640ab19203737e7bb23c634d8
nuke1115 <nuclearbomb280@gmail.com>
2024-04-28 14:04:46 +0900

    solved : 1037

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/1037.cs
@@ -0,0 +1,40 @@
+﻿namespace Fuc31
+{
+	public class Program
+	{
+		public static void aMain()
+		{
+			int num = int.Parse(Console.ReadLine());
+			string[] input = Console.ReadLine().Split(" ");
+
+			int lowestNum = 1000001;
+			int highestNum = int.Parse(input[0]);
+			int tmp = 0;
+
+
+
+
+			for (int i = 0; i < num; i++)
+			{
+				tmp = int.Parse(input[i]);
+
+
+				if (tmp < lowestNum)
+				{
+					lowestNum = tmp;
+				}
+				
+				if (tmp > highestNum)
+				{
+					highestNum = tmp;
+				}
+
+			}
+			
+			Console.WriteLine(lowestNum*highestNum);
+
+
+
+		}
+	}
+}
