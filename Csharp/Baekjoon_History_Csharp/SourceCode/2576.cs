b2a133c8c5e5425296126fa95248c48313ff9724
nuke1115 <nuclearbomb280@gmail.com>
2024-05-02 12:28:55 +0900

    solved : 2576

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/2576.cs
@@ -0,0 +1,33 @@
+﻿namespace Fuc32
+{
+	public class Program
+	{
+		const int oddCheck = 1;
+		public static void aMain()
+		{
+			int sum = 0;
+			int smallestOdd = 101;
+			int tmp = 0;
+
+			for (int i = 0; i < 7; i++)
+			{
+				tmp = int.Parse(Console.ReadLine());
+				if ((tmp & oddCheck) == 1)
+				{
+					sum += tmp;
+					smallestOdd = smallestOdd > tmp ? tmp : smallestOdd;
+				}
+			}
+
+			if (sum == 0)
+			{
+				Console.WriteLine(-1);
+			}
+			else
+			{
+				Console.WriteLine($"{sum}\n{smallestOdd}");
+			}
+			
+		}
+	}
+}
