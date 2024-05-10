fa9c7993cc30e9004cd7529caab4ae6e3582d692
nuke1115 <nuclearbomb280@gmail.com>
2024-05-11 01:33:17 +0900

    solved : 1676

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/1676.cs
@@ -0,0 +1,23 @@
+﻿namespace Fuc34k
+{
+	public class Program
+	{
+		public static void aMain()
+		{
+			int num = int.Parse(Console.ReadLine());
+			int num2 = num;
+			int num3 = num;
+
+			num /= 5;
+			num2 /= 25;
+			num3 /= 125;
+
+			Console.WriteLine(num+num2+num3);
+		}
+		
+	}
+}
+/*
+1:  n!에서 5가 더해질떄마다 0이 하나씩 는다
+2: n이 5^k의 배수가 되는 주기는 k씩 는다
+ */
