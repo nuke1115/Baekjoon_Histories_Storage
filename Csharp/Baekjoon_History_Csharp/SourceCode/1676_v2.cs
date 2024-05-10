932a0bb9e2e70102919a93e2e5b5a2af5eace460
nuke1115 <nuclearbomb280@gmail.com>
2024-05-11 01:58:30 +0900

    solved : 1676 - v2

---
+++ Csharp/Baekjoon_History_Csharp/SourceCode/1676_v2.cs
@@ -0,0 +1,34 @@
+﻿namespace Fuc34_2
+{
+	public class Program
+	{
+		public static void aMain(string[] args)
+		{
+			int two = 0;
+			int five = 0;
+			int num = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i <= num; i++)
+			{
+				int fu = i;
+				int ck = i;
+
+				while (fu % 2 == 0)
+				{
+					two++;
+					fu /= 2;
+				}
+
+				while (ck % 5 == 0)
+				{
+					five++;
+					ck /= 5;
+				}
+
+			}
+
+			Console.WriteLine(two < five ? two : five);
+
+		}
+	}
+}
